c8a6fed806dcc714fe5e41697e5587712f4faff8
Aamir Ali <aa882755@gmail.com>
2024-08-24 21:36:04 +0530

    Keywords -> Static

--- csharp-concept/Program.cs
+++ csharp-concept/Program.cs
@@ -89,10 +89,10 @@ var serviceProvider = serviceCollection.BuildServiceProvider();
 
 //////OOPS//////
 ///Encapsulation
-//ChickenProgram.ChickenMain();
-//Car_ReadOnlyProgram.Car_ReadOnlyMain();
-//Person_Get_and_Set_Program.Person_Get_and_Set_Main();
-//PasswordManager_OnlySetter_Program.PasswordManager_OnlySetter_Main();
+ChickenProgram.ChickenMain();
+Car_ReadOnlyProgram.Car_ReadOnlyMain();
+Person_Get_and_Set_Program.Person_Get_and_Set_Main();
+PasswordManager_OnlySetter_Program.PasswordManager_OnlySetter_Main();
 
 
 ///Interface
---
+++ csharp-concept/keywords/statickeyword/UtilityProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace csharp_concept.keywords.statickeyword;
+
+public static class Utility
+{
+    static Utility()
+    {
+        // Static constructor to initialize static members
+        Console.WriteLine("Static constructor called");
+    }
+
+    public static void DoSomething()
+    {
+        Console.WriteLine("Doing something");
+    }
+}
